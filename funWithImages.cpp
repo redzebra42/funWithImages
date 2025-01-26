36d1554828b62389a4d74b6c0bd3218bbc9a2e62
Tristan Delaigue <delaigletristan42@gmail.com>
2025-01-26 19:22:25 +0100

    first commit

---
+++ funWithImages.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    if (argc == 0)
+    {
+        cerr << "main [image file name] [destination file name]" << endl;
+    }
+    else
+    {
+        
+    }
+    return 0;
+}
+
+
---
+++ imageUtils.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
